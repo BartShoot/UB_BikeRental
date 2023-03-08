169f1e896426a50d85573568e80c2e12e39b0770
Bartosz Dobija <33185107+BartShoot@users.noreply.github.com>
2023-03-08 16:44:21 +0100

    commit

--- UB_BikeRental/Program.cs
+++ UB_BikeRental/Program.cs
@@ -1,7 +1,22 @@
-namespace UB_BikeRental
+﻿namespace UB_BikeRental
 {
     public class Program
     {
+        /*
+        Utworzyć projekt, który będzie stopniowo rozbudowywany
+        Wykonać modyfikację layoutu
+            Wprowadzić nagłówek portalu (ATH Bike Rental System)
+            Stworzyć stopkę z informacjami kontaktowymi
+        Przygotować widok domyślny z informacjami o portalu
+            Z wyeksponowaną nazwą
+            Opisem działania
+            Przykładowymi zdjęciami
+        Przygotować ViewModele do Obsługi Listy Pojazdów
+            VehicleDetailViewModel – do prezentacji szczegółów pojedynczego pojazdu
+            VehicleItemViewModel – do prezentacji pozycji na liście pojazdów
+        Przygotować Controller Vehicles i Widoki do prezentacji listy Pojazdów i Danych dla wybranego Pojazdu
+        Zaprezentować Widoki dla danych testowych
+        */
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
